9e69473c219f98560e578cc4b5e3f9c328214c91
Endorsmoch <A16003872@alumnos.uady.mx>
2023-03-02 13:20:24 -0600

    Loginfirebase recuperacion

---
+++ diabetic_app/lib/widget_tree.dart
@@ -0,0 +1,27 @@
+import 'package:diabetic_app/auth.dart';
+import 'package:diabetic_app/pages/home_page.dart';
+import 'package:diabetic_app/pages/login_register_page.dart';
+import 'package:flutter/material.dart';
+
+class WidgetTree extends StatefulWidget {
+  const WidgetTree({Key? key}) : super(key: key);
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return HomePage();
+          } else {
+            return const LoginPage();
+          }
+        }
+    );
+  }
+}
