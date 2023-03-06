0aa4b7d186d8c5fceaefb4af86b84878277dc73a
Endorsmoch <A16003872@alumnos.uady.mx>
2023-03-06 15:39:04 -0600

    google and fb pub spec got

--- diabetic_app/lib/auth.dart
+++ diabetic_app/lib/auth.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class Auth {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
@@ -29,6 +31,23 @@ class Auth {
 
   Future<void> signOut() async {
     await _firebaseAuth.signOut();
+  }
+
+  Future<UserCredential> signInWithFacebook() async {
+    final LoginResult _loginResult = await FacebookAuth.instance.login();
+    final OAuthCredential facebookAuthCredential = FacebookAuthProvider.credential(_loginResult.accessToken!.token);
+    return _firebaseAuth.signInWithCredential(facebookAuthCredential);
+  }
+
+  Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    return await _firebaseAuth.signInWithCredential(credential);
   }
 
 }
